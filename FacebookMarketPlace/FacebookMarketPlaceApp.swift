a2f69ad7149f05b4ab83bf6ab534ea3c0b2bd45b
bajajku <bajajku@sheridancollege.ca>
2025-01-31 10:07:09 -0500

    Initial Commit

---
+++ FacebookMarketPlace/FacebookMarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookMarketPlaceApp.swift
+//  FacebookMarketPlace
+//
+//  Created by Kunal Bajaj on 2025-01-31.
+//
+
+import SwiftUI
+
+@main
+struct FacebookMarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FacebookMarketPlaceTests/FacebookMarketPlaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookMarketPlaceTests.swift
+//  FacebookMarketPlaceTests
+//
+//  Created by Kunal Bajaj on 2025-01-31.
+//
+
+import Testing
+@testable import FacebookMarketPlace
+
+struct FacebookMarketPlaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
